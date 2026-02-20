eb6ce94cafc2f5e87ac923b5e7e551badf5c5d2b
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2026-02-20 19:27:22 +0000

    JOB-2 Created register route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./features/auth/register/register.component')
+        .then(c => c.RegisterComponent)
+  }
+];

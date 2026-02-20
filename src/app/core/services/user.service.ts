a9ec29cf2d7c5d3d852e4d104a0a4039584540b4
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2026-02-20 19:26:28 +0000

    JOB-2 Created user service with register

---
+++ src/app/core/services/user.service.ts
@@ -0,0 +1,40 @@
+import {inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {RegisterUser} from '../../shared/models/registeruser';
+import {map, Observable} from 'rxjs';
+import {SafeUser} from '../../shared/models/safeuser';
+import {User} from '../../shared/models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private readonly http = inject(HttpClient);
+  private API_URL: string = environment.baseApiUrl+'/users';
+
+  register(user: RegisterUser): Observable<SafeUser> {
+    return this.http.post<User>(this.API_URL, user).pipe(
+      map(res => ({
+        id: res.id,
+        fname: res.fname,
+        lname: res.lname,
+        email: res.email
+      }))
+    );
+  }
+
+  loadByEmail(email: string): Observable<SafeUser | null> {
+    return this.http.get<SafeUser[]>(
+      `${this.API_URL}?email=${encodeURIComponent(email)}`
+    ).pipe(
+      map(res => res.length > 0 ? {
+          id: res[0].id,
+          fname: res[0].fname,
+          lname: res[0].lname,
+          email: res[0].email
+        } : null
+      )
+    );
+  }
+}

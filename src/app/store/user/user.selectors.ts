39fa210398613dfe1f51b427e8819969d01cce45
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2026-02-21 17:24:22 +0000

    JOB-14 Created selector for isAuth

--- src/app/store/user/user.selectors.ts
+++ src/app/store/user/user.selectors.ts
@@ -6,3 +6,6 @@ export const selectUserState = createFeatureSelector<UserState>('user');
 export const selectUserLoading = createSelector(selectUserState, state => state.loading);
 export const selectUserError = createSelector(selectUserState, state => state.error);
 export const selectUser = createSelector(selectUserState, state => state.user);
+
+export const selectIsAuthenticated = createSelector(selectUserState, state => state.user !== null);
+

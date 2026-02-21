ebce48799d51e63d8c1f1c752006845b8a42f440
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2026-02-21 17:25:09 +0000

    JOB-14 Implemented guards in routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,17 @@
 import { Routes } from '@angular/router';
+import {guestGuard} from './core/guards/guest.guard';
 
 export const routes: Routes = [
   {
     path: 'register',
+    canActivate: [guestGuard],
     loadComponent: () =>
       import('./features/auth/register/register.component')
         .then(c => c.RegisterComponent)
   },
   {
     path: 'login',
+    canActivate: [guestGuard],
     loadComponent: () =>
       import('./features/auth/login/login.component')
         .then(c => c.LoginComponent)

4bd26c461e8284307777d628db4a1a2e08e3c424
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2026-02-22 22:41:52 +0000

    updated app config and routes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,13 +7,16 @@ import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { UserReducer, UserEffects } from './store';
+import { jobReducer, JobEffects } from './store/job';
+import { favoriteReducer, FavoriteEffects } from './store/favorite';
+import { trackApplicationReducer, TrackApplicationEffects } from './store/track-application';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideStore({ user: UserReducer }),
-    provideEffects([UserEffects]),
+    provideStore({ user: UserReducer, jobs: jobReducer, favorites: favoriteReducer, trackApplications: trackApplicationReducer }),
+    provideEffects([UserEffects, JobEffects, FavoriteEffects, TrackApplicationEffects]),
     provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
     provideHttpClient()
   ]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,40 @@
 import { Routes } from '@angular/router';
 import {guestGuard} from './core/guards/guest.guard';
+import {authGuard} from './core/guards/auth.guard';
 
 export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'jobs',
+    pathMatch: 'full'
+  },
+  {
+    path: 'jobs',
+    loadComponent: () =>
+      import('./features/search/jobs/jobs.component')
+        .then(c => c.JobsComponent)
+  },
+  {
+    path: 'favorites',
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import('./features/favorite/favorite.component')
+        .then(c => c.FavoriteComponent)
+  },
+  {
+    path: 'track',
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import('./features/track-application/track-application.component')
+        .then(c => c.TrackApplicationComponent)
+  },
+  {
+    path: 'profile',
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import('./features/profile/profile.component')
+        .then(c => c.ProfileComponent)
+  },
   {
     path: 'register',
     canActivate: [guestGuard],
